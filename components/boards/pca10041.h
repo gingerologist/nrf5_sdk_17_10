86d473b70729ab5b174ac0d1d8f737582f2a54b1
matianfu <matianfu@gingerologist.com>
2023-07-26 22:38:30 +0800

    update button and led in board file.

--- components/boards/pca10041.h
+++ components/boards/pca10041.h
@@ -46,51 +46,41 @@ extern "C" {
 
 #include "nrf_gpio.h"
 
-// LEDs definitions for PCA10040
-#define LEDS_NUMBER    4
+// LEDs definitions for PCA10041
+#define LEDS_NUMBER         2
 
-#define LED_START      17
-#define LED_1          17
-#define LED_2          18
-#define LED_3          19
-#define LED_4          20
-#define LED_STOP       20
+#define LED_START           9
+#define LED_1               9
+#define LED_2               10
+#define LED_STOP            10
 
 #define LEDS_ACTIVE_STATE 0
 
 #define LEDS_INV_MASK  LEDS_MASK
 
-#define LEDS_LIST { LED_1, LED_2, LED_3, LED_4 }
+#define LEDS_LIST { LED_1, LED_2 }
 
 #define BSP_LED_0      LED_1
 #define BSP_LED_1      LED_2
-#define BSP_LED_2      LED_3
-#define BSP_LED_3      LED_4
 
-#define BUTTONS_NUMBER 4
+#define BUTTONS_NUMBER 1
 
-#define BUTTON_START   13
-#define BUTTON_1       13
-#define BUTTON_2       14
-#define BUTTON_3       15
-#define BUTTON_4       16
-#define BUTTON_STOP    16
+#define BUTTON_START   25
+#define BUTTON_1       25
+#define BUTTON_STOP    25
 #define BUTTON_PULL    NRF_GPIO_PIN_PULLUP
 
 #define BUTTONS_ACTIVE_STATE 0
 
-#define BUTTONS_LIST { BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4 }
+#define BUTTONS_LIST        { BUTTON_1 }
 
-#define BSP_BUTTON_0   BUTTON_1
-#define BSP_BUTTON_1   BUTTON_2
-#define BSP_BUTTON_2   BUTTON_3
-#define BSP_BUTTON_3   BUTTON_4
+#define BSP_BUTTON_0        BUTTON_1
 
 #define RX_PIN_NUMBER  8
 #define TX_PIN_NUMBER  6
-#define CTS_PIN_NUMBER 7
-#define RTS_PIN_NUMBER 5
-#define HWFC           true
+// #define CTS_PIN_NUMBER 7
+// #define RTS_PIN_NUMBER 5
+#define HWFC           false
 
 #define SPIS_MISO_PIN   28  // SPI MISO signal.
 #define SPIS_CSN_PIN    12  // SPI CSN signal.
