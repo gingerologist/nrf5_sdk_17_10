f096570ff656fd8effbbcac9a81097df794cbc07
matianfu <matianfu@gingerologist.com>
2023-07-26 23:58:26 +0800

    add eeg service, freertos and all dependencies

--- examples/ble_peripheral/ble_app_sleepmon/pca10041/s132/config/app_config.h
+++ examples/ble_peripheral/ble_app_sleepmon/pca10041/s132/config/app_config.h
@@ -22,33 +22,33 @@
 
 #define NRFX_SAADC_ENABLED                              1
 #define NRFX_SAADC_CONFIG_RESOLUTION                    3 // 14 bit
-#define SAADC_ENABLED   1
-#define SAADC_CONFIG_RESOLUTION 3
+#define SAADC_ENABLED                                   1
+#define SAADC_CONFIG_RESOLUTION                         3
 
-#define NRFX_SPIM_ENABLED   1
-#define SPI_ENABLED 1
-#define SPI0_ENABLED 1
-#define SPI0_USE_EASY_DMA 0
+#define NRFX_SPIM_ENABLED                               1
+#define SPI_ENABLED                                     1
+#define SPI0_ENABLED                                    1
+#define SPI0_USE_EASY_DMA                               0
 
-#define NRFX_TWIM_ENABLED   1
-#define NRFX_TWI_ENABLED    1
-#define TWI_ENABLED 1
-#define TWI1_ENABLED 1
-#define NRF_TWI_MNGR_ENABLED 1
+#define NRFX_TWIM_ENABLED                               1
+#define NRFX_TWI_ENABLED                                1
+#define TWI_ENABLED                                     1
+#define TWI1_ENABLED                                    1
+#define NRF_TWI_MNGR_ENABLED                            1
 
 // are these necessary?
-#define HCI_UART_RX_PIN 0
-#define HCI_UART_TX_PIN 0
-#define HCI_UART_RTS_PIN 0
-#define HCI_UART_CTS_PIN 0
+#define HCI_UART_RX_PIN                                 0
+#define HCI_UART_TX_PIN                                 0
+#define HCI_UART_RTS_PIN                                0
+#define HCI_UART_CTS_PIN                                0
 
-#define NRF_QUEUE_ENABLED 1
+#define NRF_QUEUE_ENABLED                               1
 
-#define NRF_SDH_BLE_GATT_MAX_MTU_SIZE 251
-#define NRF_SDH_BLE_VS_UUID_COUNT       1
+#define NRF_SDH_BLE_GATT_MAX_MTU_SIZE                   251
+#define NRF_SDH_BLE_VS_UUID_COUNT                       1
 
-#define NRF_SDH_BLE_SERVICE_CHANGED     0   // no idea
+#define NRF_SDH_BLE_SERVICE_CHANGED                     0   // no idea
 
-#define NRF_SDH_DISPATCH_MODEL          0   // 0 interrupt, 2 polling (freertos must use 2)
+#define NRF_SDH_DISPATCH_MODEL                          2   // 0 interrupt, 2 polling (freertos must use 2)
 
 #endif
