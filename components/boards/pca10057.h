81eedec19e5b5dad1d80fdfaf8489a391d9c9ba3
matianfu <matianfu@gingerologist.com>
2023-09-07 13:44:12 +0800

    reconfig tx/rx gpio in board file

--- components/boards/pca10057.h
+++ components/boards/pca10057.h
@@ -84,8 +84,8 @@ extern "C" {
 #define BSP_BUTTON_2   BUTTON_3
 #define BSP_BUTTON_3   BUTTON_4
 
-#define RX_PIN_NUMBER  26                           // 8
-#define TX_PIN_NUMBER  27                           // 6
+#define RX_PIN_NUMBER  (32 + 14)                    // 8
+#define TX_PIN_NUMBER  (32 + 13)                    // 6
 #define CTS_PIN_NUMBER NRF_UART_PSEL_DISCONNECTED   // 7
 #define RTS_PIN_NUMBER NRF_UART_PSEL_DISCONNECTED   // 5
 #define HWFC           false                        // true
